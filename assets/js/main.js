788cd657e8e3d205a2929960fd80c03806608363
dohy-9443 <dong_hyun95@hanmail.net>
2021-08-04 18:14:37 +0900

    scroll up , scroll bar

--- assets/js/main.js
+++ assets/js/main.js
@@ -88,3 +88,43 @@ function finalVideo() {
 }
 
 videoFile.addEventListener("ended", finalVideo);
+
+// show scroll top
+function scrollUp() {
+  const scrollUp = document.querySelector("#scroll-up");
+
+  if (this.scrollY >= 200) {
+    scrollUp.classList.add("show-scroll");
+  } else {
+    scrollUp.classList.remove("show-scroll");
+  }
+}
+
+window.addEventListener("scroll", scrollUp);
+
+// scroll sections active link
+const sections = document.querySelectorAll("section[id]");
+
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
+  });
+}
+
+window.addEventListener("scroll", scrollActive);
+
+// dark light theme
