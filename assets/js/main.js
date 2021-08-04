2609cbb5405a871c952565b574cf41f1b24272e8
dohy-9443 <dong_hyun95@hanmail.net>
2021-08-04 15:51:29 +0900

    change background header

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,3 +25,16 @@ function linkAction() {
 }
 
 navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+// change background header
+function scrollHeader() {
+  const header = document.querySelector("#header");
+
+  if (this.scrollY >= 100) {
+    header.classList.add("scroll-header");
+  } else {
+    header.classList.remove("scroll-header");
+  }
+}
+
+window.addEventListener("scroll", scrollHeader);
